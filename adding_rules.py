b717c88055e4a59d6383d57921f53a01b748de83
Iuliia Belokon <75740030+harmar14@users.noreply.github.com>
2024-03-10 23:18:09 +0300

    Update adding_rules.py

--- adding_rules.py
+++ adding_rules.py
@@ -4,26 +4,27 @@ from spacy.pipeline import EntityRuler
 import json
 
 PROJECTS_JSON = "./data/projects.json"
+WEEKDAYS_JSON = "./data/weekdays.json"
 
 def load_data(file):
     # Функция получает данные из json-файла.
-    with open(file, "r") as f:
+    with open(file, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
 
-def generate_better_projects(file):
+def generate_better_patterns(file):
     # Функция учитывает вариации написания названий проектов.
     data = load_data(file)
-    new_projects = []
+    new_patterns = []
     for item in data:
-        new_projects.append(item)
-        new_projects.append(item.lower())
-        new_projects.append(item.upper())
-    return new_projects
+        new_patterns.append(item)
+        new_patterns.append(item.lower())
+        new_patterns.append(item.upper())
+    return new_patterns
 
 def create_training_data(file, type):
     # Функция подготовки данных для отправки в модель.
-    data = generate_better_projects(file)
+    data = generate_better_patterns(file)
     patterns = []
     for item in data:
         pattern = {"label":type, "pattern":item}
@@ -44,8 +45,12 @@ def test_model(model, text):
         results.append([ent.text, ent.label_])
     return results
 
-#patterns = create_training_data(PROJECTS_JSON, "PROJECT")
-#generate_rules(patterns)
+projects_patterns = create_training_data(PROJECTS_JSON, "PROJECT")
+#print(projects_patterns)
+weekdays_patterns = create_training_data(WEEKDAYS_JSON, "WEEKDAY")
+#print(weekdays_patterns)
+
+generate_rules([*projects_patterns, *weekdays_patterns])
 
 # Тест модели.
 nlp = spacy.load("model_with_rules")
