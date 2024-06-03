3ed766a9837e32fe47523fb710a8753bb779d432
Iuliia Belokon <75740030+harmar14@users.noreply.github.com>
2024-06-04 00:32:15 +0300

    Update json_to_spacy.py

--- json_to_spacy.py
+++ json_to_spacy.py
@@ -1,4 +1,5 @@
 import json
+import re
 import spacy
 from spacy.tokens import DocBin
 
@@ -15,16 +16,33 @@ def convert_data(json_data):
         print(text)
         print(annotations)        
         doc = nlp.make_doc(text)
+        # NER.
         ents = []
+        isUrgent = False
         for start, end, label in annotations["entities"]:
             span = doc.char_span(start, end, label=label)
             if span is None:
                 print("Skipping entity")
             else:
                 ents.append(span)
+                if ( label == "DATE" or label == "WEEKDAY" ):
+                    isUrgent = True
+                elif ( label == "POSITION" ):
+                    checkEnt = re.findall("руководитель", text[start:end], re.I)
+                    if ( len(checkEnt) > 0 ):
+                        isUrgent = True                    
         doc.ents = ents
+        print(ents)
+        print(isUrgent)
+        # Classification.
+        if ( isUrgent ):
+            doc.cats["positive"] = 1
+            doc.cats["negative"] = 0
+        else:
+            doc.cats["positive"] = 0
+            doc.cats["negative"] = 1           
         db.add(doc)
-    db.to_disk("./data/dev.spacy")
+    db.to_disk("./data/train_2.spacy")
 
-TRAIN_DATA = load_data("./data/dev_data_set.json")
+TRAIN_DATA = load_data("./data/training_data_set.json")
 convert_data(TRAIN_DATA)
